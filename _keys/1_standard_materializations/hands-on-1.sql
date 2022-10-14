b2a350f640bf2bead3d188a461aafd3959009d89
Logan Cochran <logan.cochran@fishtownanalytics.com>
2022-10-14 19:35:35 +0000

    updated keys

--- _keys/1_standard_materializations/hands-on-1.sql
+++ _keys/1_standard_materializations/hands-on-1.sql
@@ -1,13 +1,11 @@
-1. You can configure credentials via the profile icon in the top right --> credentials 
+1. located at models/marts/snowplow/snowplow__page_views.sql
 
-2. located at models/marts/snowplow/snowplow__page_views.sql
-
-3. This can be done in 2 locations: dbt_project.yml file or in the model itself. We 
+2. This can be done in 2 locations: dbt_project.yml file or in the model itself. We 
 recommend you manage materializations in the dbt_project.yml file. Open the file, scroll
 to the bottom and identify the "materialized:" statement. Update the materialization of marts
 to table and keep the materializaiton of staging as view
 
-4. dbt run
+3. dbt run
 
-5. located in Snowflake under your target schema
+4. located in Snowflake under your target schema
 
